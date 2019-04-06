bf25c8af2be6cce5bff6e6a592c84c1f6a7f4a88
ivanperez_81 <ivanperez_81@hotmail.com>
2019-04-06 12:06:50 +0200

    =Adding aditional authentication methods in Controller

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +42,7 @@ public class IndexController {
 		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
 		return authorities.contains(new SimpleGrantedAuthority(role));
 		
+		// Otra forma de hacerlo, recorriendo la colección con un for
 //		for (GrantedAuthority authority : authorities) {
 //			if (role.equals(authority.getAuthority())) {
 //				return true;
@@ -49,7 +53,8 @@ public class IndexController {
 	}
 	
 	@GetMapping("/")
-	public String index(Model model, Authentication authentication) {
+	public String index(Model model, Authentication authentication, HttpServletRequest request) {
+		// User authentication check - Authentication
 		if (authentication != null) {
 			logger.info("[FROM Authentication injectable] User authenticated: '" + authentication.getName() + "'");
 		}
@@ -64,6 +69,17 @@ public class IndexController {
 		}
 		else {
 			logger.info("[CHECK ROLE] Your role is not 'ADMIN'");
+		}
+		
+		// User authentication check - SecurityContextHolderAwareRequestWrapper
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request, "ROLE_");
+		if (securityContext.isUserInRole("ADMIN")) {
+			logger.info("[FROM SecurityContextHolderAwareRequestWrapper] User authenticated: '" + authentication.getName() + "'");
+		}
+		
+		// User authentication check - HttpServletRequest
+		if (request.isUserInRole("ROLE_ADMIN")) {
+			logger.info("[FROM HttpServletRequest] User authenticated: '" + authentication.getName() + "'");
 		}
 		
 		model.addAttribute("title", title);
